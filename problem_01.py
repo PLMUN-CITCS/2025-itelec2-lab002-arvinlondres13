5b545d14b083c78e6f2f286770f3b9d96bddc728
arvinlondres13 <arvinlondres_bsit@plmun.edu.ph>
2025-02-10 17:34:40 +0800

    problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,16 +1,15 @@
-# arvin
+# YOUR NAME
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    
     print("Simple Calculator Program")
-
+    
     num1 = int(input("Enter 1st number: "))
     
     num2 = int(input("Enter 2nd number: "))
-
+    
     print(f"The sum is {num1 + num2}")
     
     print(f"The difference is {num1 - num2}")
