2b4bc6d50f411b7af6f406e7ef639a9d4d85c625
arvinlondres13 <arvinlondres_bsit@plmun.edu.ph>
2025-02-10 16:40:33 +0800

    problem _01.py

--- problem _01.py
+++ problem _01.py
@@ -1,10 +1,9 @@
-# londresarvin
+# arvinlondres
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    
     print("Simple Calculator Program")
     
     num1 = int(input("Enter 1st number: "))
