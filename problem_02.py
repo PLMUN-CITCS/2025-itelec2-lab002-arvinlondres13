b6ff965bfe8dd155a38644c1c3ae5e92e8a97830
arvinlondres13 <arvinlondres_bsit@plmun.edu.ph>
2025-02-10 17:18:42 +0800

    problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,14 +1,15 @@
-# londresarvin
+
+# YOUR NAME
 # ITELEC2
-# Problem Set 01 - Problem 02
-# Square the Number Program
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
 
 def main():
     print("Square the Number Program")
-
-    number = int(input("Enter a number: "))
-
-    print(f"The square of {number} is {(number * number):.2f}")
     
+    number = int(input("Enter a number: "))
+    
+    print(f"The square of {number} is {(number * number):.2f}")
+
 if __name__ == "__main__":
     main()
